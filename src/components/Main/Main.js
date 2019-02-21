50225028789e659dbda0b57cfca27e58f9b60148
kai <kinishino1fuka2@outlook.jp>
2019-02-21 23:24:55 +0900

    Main(親コンポーネント)からSearch(子コンポーネント)にコードを移して、記述量を減らした

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -46,25 +46,6 @@ class Main extends Component {
     }
   }
 
-
-  // チェックボックスにチェックをつける
-  updateConfig(option = {}) {
-    console.log(option.category)
-    console.log(option.index)
-    let config = this.state.config
-    switch(option.category){
-      case "place":
-      case "attribute":
-        config[ option.category ][ option.index ].value = !config[ option.category ][ option.index ].value
-        this.setState({
-          config: config
-        })
-      break
-      default:
-      break
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -73,62 +54,8 @@ class Main extends Component {
             <div className="pane">
               <Map />
             </div>
-              {/*<Search />*/}
-
-            <div className="pane">
-                {/*場所から探す*/}
-                <section className="nav-section">
-                  <h2 className="nav-section-hd">場所から探す</h2>
-                  <ul className="nav-list">
-                    {
-                      this.state.config.place.map((data,i)=>{
-                        return (
-                          <li
-                            key={`place${i}`}
-                            className="nav-row checkbox"
-                            onClick={
-                              ()=>{ this.updateConfig({
-                                category: "place",
-                                index: i
-                              })}
-                            }
-                            data-checked={data.value}>
-                            <input type="checkbox" className="checkbox" />
-                            {data.label}
-                          </li>
-                        )
-                      })
-                    }
-                  </ul>
-                </section>
-
-                {/*属性から探す*/}
-                <section className="nav-section">
-                  <h2 className="nav-section-hd">属性から探す</h2>
-                  <ul className="nav-list">
-                    {
-                      this.state.config.attribute.map((data,i)=>{
-                        return (
-                          <li
-                            key={`option${i}`}
-                            className="nav-row checkbox"
-                            onClick={
-                              ()=> { this.updateConfig({
-                                category: "attribute",
-                                index: i
-                              })}
-                            }
-                            data-checked={data.value}>
-                            <input type="checkbox" className="checkbox" />
-                            {data.label}
-                          </li>
-                        )
-                      })
-                    }
-                  </ul>
-                </section>
-              </div>
-            </div>
+              <Search />
+          </div>
             <Link to={`/detail/aaa/`}>詳細ページへ</Link>
       </div>
     );
