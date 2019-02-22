0c5b47368dc50cde293f0a325296b3e0af7e77b3
kai <kinishino1fuka2@outlook.jp>
2019-02-23 01:20:36 +0900

    属性による絞り込みを実装した

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -20,18 +20,18 @@ class Main extends Component {
       name: "野宿びより",
 
       spot: [
-        { id: 1, name: "道の駅五霞", address: "〒306-0304 茨城県猿島群五霞町幸主18-1", place: "関東", area: "kanto",  image: 0 , score: 5, let: 0, lng: 0, hasToilet: true, hasRoof: true, hasBench: true, lat: 36.115163, lng: 139.734986},
-        { id: 2, name: "道の駅 YOU・遊・もり", address: "〒049-2311 北海道茅部郡森町上台町326-18", place: "北海道", area: "hokkaido" ,image: 0, score: 5, let: 0, lng: 0, hasToilet: true, hasRoof: true, hasBench: false, lat: 42.102251, lng: 140.568183},
-        { id: 3, name: "道の駅 厳美渓谷", address: "〒021-0101 岩手県一関市厳美町沖野々220-1", place: "東北", area: "tohoku",image: 0, score: 5, let: 0, lng: 0, hasToilet: true, hasRoof: true, hasBench: true, lat: 38.946439, lng: 141.052885},
-        { id: 4, name: "道の駅　きららあじす", address: "〒754-1277　山口県山口市阿知須509-88", place: "中国", area: "tyugoku",image: 0, score: 5, let: 0, lng: 0, hasToilet: true, hasRoof: true, hasBench: false, lat: 34.012855, lng: 131.369561},
-        { id: 5, name: "道の駅 させぼっくす", address: "〒858-0917 長崎県佐世保市愛宕町11", place: "九州・沖縄", area: "kyusyuokinawa",image: 0, score: 5, let: 0, lng: 0, hasToilet: true, hasRoof: true, hasBench: true, lat: 33.201749, lng: 129.671847}
+        { id: 1, name: "道の駅五霞", address: "〒306-0304 茨城県猿島群五霞町幸主18-1", place: "関東", area: "kanto",  image: 0 , score: 5, hasToilet: 1, hasRoof: 1, hasBench: 1, lat: 36.115163, lng: 139.734986},
+        { id: 2, name: "道の駅 YOU・遊・もり", address: "〒049-2311 北海道茅部郡森町上台町326-18", place: "北海道", area: "hokkaido" ,image: 0, score: 5, hasToilet: 1, hasRoof: 1, hasBench: 0, lat: 42.102251, lng: 140.568183},
+        { id: 3, name: "道の駅 厳美渓谷", address: "〒021-0101 岩手県一関市厳美町沖野々220-1", place: "東北", area: "tohoku",image: 0, score: 5, hasToilet: 1, hasRoof: 1, hasBench: 0, lat: 38.946439, lng: 141.052885},
+        { id: 4, name: "道の駅　きららあじす", address: "〒754-1277　山口県山口市阿知須509-88", place: "中国", area: "tyugoku",image: 0, score: 5, hasToilet: 1, hasRoof: 1, hasBench: 0, lat: 34.012855, lng: 131.369561},
+        { id: 5, name: "道の駅 させぼっくす", address: "〒858-0917 長崎県佐世保市愛宕町11", place: "九州・沖縄", area: "kyusyuokinawa",image: 0, score: 5, hasToilet: 1, hasRoof: 1, hasBench: 1, lat: 33.201749, lng: 129.671847}
       ],
 
       selectedSpot: null, // 詳細,レビュー画面に対応するspotをセットする
       config: {
         place: [
           { label: "北海道", key: "hokkaido", value: false },
-          { label: "東北", key: "touhoku", value: false },
+          { label: "東北", key: "tohoku", value: false },
           { label: "関東", key: "kanto", value: false },
           { label: "北陸・甲信越", key: "hokurikukousinetu", value: false },
           { label: "東海", key: "tokai", value: false },
@@ -70,7 +70,7 @@ class Main extends Component {
               var mapLatLng = new window.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
               // マップオプションを変数に格納
               var mapOptions = {
-                zoom : 15,          // 拡大倍率
+                zoom : 7,          // 拡大倍率
                 center : mapLatLng  // 緯度・経度
               };
               // マップオブジェクト作成
@@ -85,14 +85,6 @@ class Main extends Component {
               });
 
               self.updateConfig()
-
-              // self.state.spot.forEach((spot)=>{
-              //   let latLng = new window.google.maps.LatLng (spot.lat, spot.lng);
-              //   let marker = new window.google.maps.Marker({
-              //     map: self.map,
-              //     position: latLng
-              //   })
-              // })
 
             },
 
@@ -154,52 +146,24 @@ class Main extends Component {
 
     // 属性による絞り込み
     let filterAtrKey = this.state.config.attribute.filter(a=>a.value).map(a=>a.key)
-    console.log(filterAtrKey)
+    console.log(filterAtrKey) // ["hasBench","hasRoof","hasToilet"]
+    let spot = this.state.spot
+    if(filterAtrKey.length > 0){
+      spot = spot.map(s=>{
+        let active = true
+        filterAtrKey.forEach(key=>{
+          if(s[key] === 0) active = false
+        })
+        s.active = active
+        return s
+      })
+    } else {
+      spot = spot.map(s=>{
+        s.active = true
+        return s
+      })
+    }
 
-    this.state.spot.forEach(s=>{
-      
-    })
-
-
-
-      // if(filterPlaceKey.length > 0){
-      //   spot = spot.map(s=>{
-      //     let active = true
-      //     if (s === filterPlaceKey) {
-      //       s.active = active
-      //     }
-      //
-      //   filterPlaceKey.forEach(key=>{
-      //     if(s[key] === 0) active = false
-      //   })
-      //   s.active = active
-      //   return s
-      //   })
-      //   } else {
-      //   spot = spot.map(s=>{
-      //     s.active = true
-      //     return s
-      //   })
-      // }
-
-
-    // 属性による絞り込み
-    // let filterAtrKey = this.state.config.attribute.filter(a=>a.value).map(a=>a.key)
-      // if(filterAtrKey.length > 0){
-      //   spot = spot.map(s=>{
-      //   let active = true
-      //   filterAtrKey.forEach(key=>{
-      //     if(s[key] === 0) active = false
-      //   })
-      //   s.active = active
-      //   return s
-      //   })
-      // } else {
-      //   spot = spot.map(s=>{
-      //     s.active = true
-      //     return s
-      //   })
-      // }
     this.putMarker()
   }
 
